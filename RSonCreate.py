fe486cdb7db7c2ddd6a10b4d0177bb0613ee2050
Liran Gazit <47790066+lirangazit@users.noreply.github.com>
2020-02-18 11:57:08 -0500

    Update RSonCreate.py

--- RSonCreate.py
+++ RSonCreate.py
@@ -256,9 +256,9 @@ def udp_broadcast(camera, Host_IP, UDP_Port_Type, queue):
             
             # Broadcast
 	        if UDP_Port_Type == "Tag":
-                s.sendto(packet, (Host_IP, Tag_UDP_Port_Number))
+		    s.sendto(packet, (Host_IP, Tag_UDP_Port_Number))
 	        if UDP_Port_Type == "Dist":
-                s.sendto(packet, (Host_IP, Dist_UDP_Port_Number))
+		    s.sendto(packet, (Host_IP, Dist_UDP_Port_Number))
                 
     except socket.error:
         # Turn off green LED
